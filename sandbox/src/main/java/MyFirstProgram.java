fa13229a07379c165e3d82671c5127f61a0835a2
D3VDCVT <safronov.eg.ol@gmail.com>
2023-01-03 02:09:33 +0300

    123

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,12 @@
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+
+		Point ab = new Point(4,2, 8, 2);
+
+
+		System.out.println("Точка А имеет координаты " + "(" + ab.x1 + "," + ab.y1 + ")");
+		System.out.println("Точка B имеет координаты " + "(" + ab.x2 + "," + ab.y2 + ")");
+		System.out.println("Длина прямой АB равна " + ab.result());
 	}
 
 }
